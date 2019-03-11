346ecb8aa0e7edef52e4345715996849f50d39b1
irabi <irabikassim01@gmail.com>
2019-03-11 11:59:36 +0300

    add navigation bar to the Navigation component

---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+
+export class Home extends Component {
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1>This is the home page.... Welcome to bofu construction limited</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Home
---
+++ src/components/Navigation.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
+
+export class Navigation extends Component {
+  render() {
+    return (
+      <div>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link className="navbar-brand" to="#">Bofu Logo</Link>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                    <li className="nav-item">
+                        <Link className="nav-link" to="#">Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="#">About</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="#">Services</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="#">Showcase</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="#">Contacts</Link>
+                    </li>
+                    </ul>
+                </div>
+            </nav>
+      </div>
+    )
+  }
+}
+
+export default Navigation
