d72fe7e446d59319645344bf4d1dddd9211251c4
irabi <irabikassim01@gmail.com>
2019-03-11 12:47:55 +0300

    add css

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react'
 
-
 export class Home extends Component {
   render() {
     return (
       <div>
-        <div className="container">
-          <div className="row">
-            <div className="col-md-12">
-              <h1>This is the home page.... Welcome to bofu construction limited</h1>
+        <div className="home">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12">
+                <h1>This is the home page.... Welcome to bofu construction limited</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="why_bofu">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-4">
+                <div className="contents">
+                  <h1>Efficiency</h1>
+                  <p>Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo,</p>
+                </div>
+              </div>
+              <div className="col-md-4">
+                <div className="contents">
+                  <h1>Integrity</h1>
+                  <p>Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo,</p>
+                </div>
+              </div>
+              <div className="col-md-4">
+                <div className="contents">
+                  <h1>Efficiency</h1>
+                  <p>Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo,</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
