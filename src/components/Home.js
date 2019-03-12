cfe7c4d0bf97a3b491af31415ca581fa00f77582
irabi <irabikassim01@gmail.com>
2019-03-12 06:51:38 +0300

    add h1 and h2 tags

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,8 @@ export class Home extends Component {
           <div className="container">
             <div className="row">
               <div className="col-md-12">
-                <h1>This is the home page.... Welcome to bofu construction limited</h1>
+                <h1>bofu construction limited</h1>
+                <h2>We build the truth</h2>
               </div>
             </div>
           </div>
