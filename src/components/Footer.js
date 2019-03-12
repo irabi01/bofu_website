f8779625aaa7ac685c736d2fa51d7bbc03793229
irabi <irabikassim01@gmail.com>
2019-03-12 15:27:24 +0300

    add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+export class Footer extends Component {
+  render() {
+    return (
+        <div className="footer_container">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="address">
+                
+              </div>
+              <hr/>
+              <div className="copyright">
+                <p><i className="fa fa-copyright"></i> 2019. All rights reserved. Bofu Construction Limited</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Footer
