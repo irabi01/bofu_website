00e531169b37d38a0cb9a1118eb8b258cfd1ad2c
irabi <irabikassim01@gmail.com>
2019-03-13 15:29:32 +0300

    add container on about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,14 +3,30 @@ import React, { Component } from 'react'
 export class About extends Component {
   render() {
     return (
-      <div className="about_page">
-        <div className="container">
-            <div className="row">
-                <div className="col-md-6">
-                    <h1>About Bofu</h1>
+      <div>
+        <div className="about_page">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6">
+                        <h1>About Bofu</h1>
+                    </div>
+                    <div className="col-md-6">
+                        <h1>Images here</h1>
+                    </div>
                 </div>
-                <div className="col-md-6">
-                    <h1>Images here</h1>
+            </div>
+        </div>
+        <div className="about_content">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6">
+                     <img src={require('../images/bg2.jpg')} className="img-fluid" alt=""/>
+                    </div>
+                    <div className="col-md-6">
+                        <div className="about_desc">
+                            <p>Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo, quando un anonimo tipografo prese una cassetta di caratteri e li assemblò per preparare un testo campione.</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
