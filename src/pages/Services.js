02f5f13a73a7460cc74118440fc877183f105b86
irabi <irabikassim01@gmail.com>
2019-03-18 16:34:41 +0300

    add services list

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react'
 export class Services extends Component {
   render() {
     return (
+    <div>
       <div className="services">
         <div className="container">
             <div className="row">
@@ -12,6 +13,29 @@ export class Services extends Component {
             </div>
         </div>
       </div>
+      <div className="service_list">
+          <div className="row">
+            <div className="col-md-6">
+              <i class="fas fa-cogs"></i>
+              <h1>Renovation</h1>
+              <p>Lorem Ipsum è un testo segnaposto utilizzato nel settore della</p>
+            </div>
+            <div className="col-md-6">
+              <img src={require('../images/bg2.jpg')} className="img-fluid" alt=""/>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-6">
+              <img src={require('../images/bg2.jpg')} className="img-fluid" alt=""/>
+            </div>
+            <div className="col-md-6">
+              <i class="fas fa-cogs"></i>
+              <h1>Renovation</h1>
+              <p>Lorem Ipsum è un testo segnaposto utilizzato nel settore della</p>
+            </div>
+          </div>
+      </div>
+    </div>
     )
   }
 }
